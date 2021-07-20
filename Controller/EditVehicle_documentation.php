ff93c85015e3d0d8af4014224b9769a07d0c03f0
socger <socger@gmail.com>
2021-07-20 13:06:26 +0200

    DOCUMENTACION
    Tener la posibilidad de escanear documentos por registro de documentación. Tanto de vehículos, como de empleados.

--- Controller/EditVehicle_documentation.php
+++ Controller/EditVehicle_documentation.php
@@ -5,7 +5,9 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
 class EditVehicle_documentation extends EditController {
-    
+
+    use \FacturaScripts\Core\Lib\ExtendedController\DocFilesTrait;
+
     public function getModelClassName() {
         return 'Vehicle_documentation';
     }
@@ -24,9 +26,20 @@ class EditVehicle_documentation extends EditController {
         return $pageData;
     }
     
+    protected function createViews()
+    {
+        parent::createViews();
+        $this->setTabsPosition('top');
+
+        $this->createViewDocFiles();
+    }
+
     // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
     protected function loadData($viewName, $view) {
         switch ($viewName) {
+            case 'docfiles':
+                $this->loadDataDocFiles($view, $this->getModelClassName(), $this->getModel()->primaryColumnValue());
+                break;
 
             // Pestaña con el mismo nombre que este controlador EditXxxxx
             case 'EditVehicle_documentation': 
@@ -39,7 +52,32 @@ class EditVehicle_documentation extends EditController {
                 $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
                 
                 break;
+            
+            default:
+                parent::loadData($viewName, $view);
+                break;
+            
         }
+    }
+
+    // Para ejecutar acciones que vienen de .html.twig
+    protected function execPreviousAction($action)
+    {
+        switch ($action) {
+            case 'add-file':
+                return $this->addFileAction();
+
+            case 'delete-file':
+                return $this->deleteFileAction();
+
+            case 'edit-file':
+                return $this->editFileAction();
+
+            case 'unlink-file':
+                return $this->unlinkFileAction();
+        }
+
+        return parent::execPreviousAction($action);
     }
     
 }
