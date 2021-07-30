4d6893adc9e855982b200994902bf07768c62f03
socger <socger@gmail.com>
2021-07-30 13:57:05 +0200

    ASISTENCIA / FICHAJES ... Crear mantenimiento.

---
+++ Model/Employee_attendance_management.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace FacturaScripts\Plugins\OpenServBus\Model; 
+
+use FacturaScripts\Core\Model\Base;
+
+class Employee_attendance_management extends Base\ModelClass {
+    use Base\ModelTrait;
+
+    public $idemployee_attendance_management;
+        
+    public $user_fecha;
+    public $user_nick;
+    public $fechaalta;
+    public $useralta;
+    public $fechamodificacion;
+    public $usermodificacion;
+    public $activo;
+    public $fechabaja;
+    public $userbaja;
+
+    public $idemployee;
+    public $origen;
+    public $fecha;
+    public $fecha_dia;
+    public $fecha_hora;
+    public $ididentification_mean;
+    public $tipoFichaje;
+    public $idabsence_reason;
+
+    public $observaciones;
+
+    
+    // función que inicializa algunos valores antes de la vista del controlador
+    public function clear() {
+        parent::clear();
+        
+        $this->activo = true; // Por defecto estará activo
+        $this->origen = 1; // 0=Externo, 1=Manual
+    }
+    
+    // función que devuelve el id principal
+    public static function primaryColumn(): string {
+        return 'idemployee_attendance_management';
+    }
+    
+    // función que devuelve el nombre de la tabla
+    public static function tableName(): string {
+        return 'employee_attendance_managements';
+    }
+
+    // Para realizar cambios en los datos antes de guardar por modificación
+    protected function saveUpdate(array $values = [])
+    {
+        // Siendo un alta o una modificación, siempre guardamos los datos de modificación
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+        
+        $this->comprobarSiActivo();
+        
+        return parent::saveUpdate($values);
+    }
+
+    // Para realizar cambios en los datos antes de guardar por alta
+    protected function saveInsert(array $values = [])
+    {
+        // Creamos el nuevo id
+        if (empty($this->idemployee_attendance_management)) {
+            $this->idemployee_attendance_management = $this->newCode();
+        }
+
+        // Rellenamos los datos de alta
+        $this->useralta = $this->user_nick; 
+        $this->fechaalta = $this->user_fecha; 
+        
+        // Siendo un alta o una modificación, siempre guardamos los datos de modificación
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+        
+        $this->comprobarSiActivo();
+        
+        return parent::saveInsert($values);
+    }
+    
+    public function test()
+    {
+        // Para evitar la inección de sql
+        $utils = $this->toolBox()->utils();
+        $this->observaciones = $utils->noHtml($this->observaciones);
+
+        // Guardamos la fecha, porque en EditEmployee_attendance_management.xml separamos el día y la hora en dos widget
+        $this->fecha = $this->fecha_dia . ' ' . $this->fecha_hora;
+
+        return parent::test();
+    }
+
+
+    // ** ********************************** ** //
+    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
+    // ** ********************************** ** //
+    private function comprobarSiActivo()
+    {
+        if ($this->activo == false) {
+            $this->fechabaja = $this->fechamodificacion;
+            $this->userbaja = $this->usermodificacion;
+        } else { // Por si se vuelve a poner Activo = true
+            $this->fechabaja = null;
+            $this->userbaja = null;
+        }
+    }
+    
+}
