03afaf5fa03ecf099c12152d3a8d292b7dd8f31e
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-04 20:43:05 +0200

    Creado el menú de servicios y mantenimiento de paradas

---
+++ Model/Stop.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace FacturaScripts\Plugins\OpenServBus\Model; 
+
+use FacturaScripts\Core\Model\Base;
+
+class Stop extends Base\ModelClass {
+    use Base\ModelTrait;
+
+    public $idstop;
+        
+    public $user_fecha;
+    public $user_nick;
+    public $fechaalta;
+    public $useralta;
+    public $fechamodificacion;
+    public $usermodificacion;
+    public $activo;
+    public $fechabaja;
+    public $userbaja;
+    public $motivobaja;
+
+    public $nombre;
+    public $ciudad;
+    public $provincia;
+    public $codpais;
+    public $codpostal;
+    public $direccion;
+    
+    public $observaciones;
+    
+    // función que inicializa algunos valores antes de la vista del controlador
+    public function clear() {
+        parent::clear();
+        
+        $this->activo = true; // Por defecto estará activo
+    }
+    
+    // función que devuelve el id principal
+    public static function primaryColumn(): string {
+        return 'idstop';
+    }
+    
+    // función que devuelve el nombre de la tabla
+    public static function tableName(): string {
+        return 'stops';
+    }
+
+    // Para realizar cambios en los datos antes de guardar por modificación
+    protected function saveUpdate(array $values = [])
+    {
+        // Siendo un alta o una modificación, siempre guardamos los datos de modificación
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+        
+        return parent::saveUpdate($values);
+    }
+
+    // Para realizar cambios en los datos antes de guardar por alta
+    protected function saveInsert(array $values = [])
+    {
+        // Creamos el nuevo id
+        if (empty($this->idstop)) {
+            $this->idstop = $this->newCode();
+        }
+
+        // Rellenamos los datos de alta
+        $this->useralta = $this->user_nick; 
+        $this->fechaalta = $this->user_fecha; 
+        
+        // Siendo un alta o una modificación, siempre guardamos los datos de modificación
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+        
+        return parent::saveInsert($values);
+    }
+    
+    public function test() {
+        // Para evitar la inyección de sql
+        $utils = $this->toolBox()->utils();
+        $this->nombre = $utils->noHtml($this->nombre);
+        $this->ciudad = $utils->noHtml($this->ciudad);
+        $this->provincia = $utils->noHtml($this->provincia);
+        $this->codpostal = $utils->noHtml($this->codpostal);
+        $this->direccion = $utils->noHtml($this->direccion);
+        
+        $this->observaciones = $utils->noHtml($this->observaciones);
+
+        return parent::test();
+    }
+
+
+    // ** ********************************** ** //
+    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
+    // ** ********************************** ** //
+    private function comprobarSiActivo()
+    {
+        $a_devolver = true;
+        
+        if ($this->activo == false) {
+            $this->fechabaja = $this->fechamodificacion;
+            $this->userbaja = $this->usermodificacion;
+            
+            if (empty($this->motivobaja)){
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
+            }
+        } else { // Por si se vuelve a poner Activo = true
+            $this->fechabaja = null;
+            $this->userbaja = null;
+            $this->motivobaja = null;
+        }
+        return $a_devolver;
+    }
+    
+}
