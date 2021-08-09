43ee9b729b13ad3244fb2e3ae7e7750ad2eebe33
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-09 18:32:33 +0200

    Modificamos service_regulars.xml y Service_regular.php para cambiar algunos nombres de campos - facturarSN ...facturar_SN - facturarIndividualmente ... facturar_agrupando - fueraDelMunicipio ... fuera_del_municipio
    Modificamos Service_regular.php 
    - function clear() para inicializar algunos valores de la vista
    - Creamos funciones nuevas que las llamamos desde function test()
      + comprobarDiasServicio()
      + comprobarFacturacion()
    
    Modificamos es_ES.json para añadir traducciones nuevas
    
    Modificamos EditService_regular.xml para completar su diseño

--- Model/Service_regular.php
+++ Model/Service_regular.php
@@ -26,6 +26,7 @@ class Service_regular extends Base\ModelClass {
     public $codcliente;
     public $idvehicle_type;
     public $idhelper;
+
     public $lunes;
     public $martes;
     public $miercoles;
@@ -34,9 +35,9 @@ class Service_regular extends Base\ModelClass {
     public $sabado;
     public $domingo;
     
-    public $facturarSN;
-    public $facturarIndividualmente;
-    public $fueraDelMunicipio;
+    public $facturar_SN;
+    public $facturar_agrupando;
+    public $fuera_del_municipio;
     
     public $hoja_ruta_origen;
     public $hoja_ruta_destino;
@@ -57,6 +58,17 @@ class Service_regular extends Base\ModelClass {
         parent::clear();
         
         $this->activo = true; // Por defecto estará activo
+
+        $this->lunes = false;
+        $this->martes = false;
+        $this->miercoles = false;
+        $this->jueves = false;
+        $this->viernes = false;
+        $this->sabado = false;
+        $this->domingo = false;
+    
+        $this->facturar_SN = true;
+        $this->facturar_agrupando = true;
     }
     
     // función que devuelve el id principal
@@ -111,7 +123,14 @@ class Service_regular extends Base\ModelClass {
                 'invalid-alphanumeric-code',
                 ['%value%' => $this->cod_servicio, '%column%' => 'cod_servicio', '%min%' => '1', '%max%' => '10']
             );
-            
+            return false;
+        }
+        
+        if ($this->comprobarDiasServicio() == false){
+            return false;
+        }
+        
+        if ($this->comprobarFacturacion() == false){
             return false;
         }
         
@@ -165,6 +184,35 @@ class Service_regular extends Base\ModelClass {
     {
         $this->useralta = $this->user_nick; 
         $this->fechaalta = $this->user_fecha; 
+    }
+
+    private function comprobarFacturacion()
+    {
+        $a_devolver = true;
+        if ( $this->facturar_SN == false and 
+             $this->facturar_agrupando = true  ) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('Si elige FACTURAR = NO, no puede elegir AGRUPANDO = SI.');
+        }
+        return $a_devolver;
+    }
+    
+    private function comprobarDiasServicio()
+    {
+        $a_devolver = true;
+        if ( $this->lunes == false and 
+             $this->martes = false and 
+             $this->miercoles = false and 
+             $this->jueves = false and 
+             $this->viernes = false and 
+             $this->sabado = false and 
+             $this->domingo = false ) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('Ya que es un servicio regular/fijo, debe de elegirme que días de la semana se va a realizar.');
+        }
+        return $a_devolver;
     }
 
 }
