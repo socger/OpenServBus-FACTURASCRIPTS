6f57d74729250db20b2a3948ea26992c53271ab1
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-10 19:54:09 +0200

    Creamos el mantenimiento de periodos de los servicios regulares

---
+++ Controller/ListService_regular_period.php
@@ -0,0 +1,93 @@
+<?php
+namespace FacturaScripts\Plugins\OpenServBus\Controller;
+
+use FacturaScripts\Core\Lib\ExtendedController\ListController;
+
+class ListService_regular_period extends ListController {
+    // Para presentar la pantalla del controlador
+    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Empleados
+    public function getPageData(): array {
+        $pageData = parent::getPageData();
+        
+        $pageData['menu'] = 'OpenServBus';
+        $pageData['submenu'] = 'Servicios';
+        $pageData['title'] = 'Serv. regulares - Periodos';
+        
+        $pageData['icon'] = 'fas fa-calendar-day';
+        
+        return $pageData;
+    }
+    
+    protected function createViews() {
+        $this->createViewService_regular_period();
+    }
+    
+    protected function createViewService_regular_period($viewName = 'ListService_regular_period')
+    {
+        $this->addView($viewName, 'Service_regular_period');
+        
+        // Opciones de búsqueda rápida
+        // $this->addSearchFields($viewName, ['cod_servicio', 'nombre']);
+        
+        // Tipos de Ordenación
+            // Primer parámetro es la pestaña
+            // Segundo parámetro es los campos por los que ordena (array)
+            // Tercer parámetro es la etiqueta a poner
+            // Cuarto parámetro, si se rellena, le está diciendo cual es el order by por defecto, y además las opciones son
+               // 1 Orden ascendente
+               // 2 Orden descendente
+        $this->addOrderBy($viewName, ['idservice_regular', 'fecha_desde', 'fecha_hasta', 'hora_desde', 'hora_hasta'], 'Por periodo', 1);
+        $this->addOrderBy($viewName, ['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
+        
+        // Filtros
+        // Filtro checkBox por campo Activo ... addFilterCheckbox($viewName, $key, $label, $field);
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... la etiqueta a mostrar al usuario
+            // $field ... el campo del modelo sobre el que vamos a comprobar
+        // $this->addFilterCheckbox($viewName, 'conductor', 'Ver sólo conductores', 'driver_yn');
+        // $this->addFilterCheckbox($viewName, 'activo', 'Ver sólo los activos', 'activo');
+
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        // Sustituimos el filtro activo (checkBox) por el filtro activo (select)
+        $activo = [
+            ['code' => '1', 'description' => 'Activos = SI'],
+            ['code' => '0', 'description' => 'Activos = NO'],
+        ];
+        $this->addFilterSelect($viewName, 'soloActivos', 'Activos = TODOS', 'activo', $activo);        
+
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        // Sustituimos el filtro activo (checkBox) por el filtro activo (select)
+        $salidaDesdeNave = [
+            ['code' => '1', 'description' => 'Salida desde nave = SI'],
+            ['code' => '0', 'description' => 'Salida desde nave = NO'],
+        ];
+        $this->addFilterSelect($viewName, 'salidaDesdeNave', 'Salida desde nave = TODOS', 'salida_desde_nave_sn', $salidaDesdeNave);        
+
+        // Filtro autoComplete ... addFilterAutocomplete($viewName, $key, $label, $field, $table, $fieldcode, $fieldtitle)
+        // Aunque lo vamos a hacer sobre la tabla empresa que normalmente tiene pocos registros
+        // este tipo de filtros está pensado para tablas como clientes, proveedores, etc que tengan muchos registros
+        // Para estas tablas no vamos a usar un filtro Select ... faltaría memoria al equipo para ello
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro, que puede ser el campo sobre el que quiero filtrar
+            // $label ...  parámetro es la etiqueta a mostrar al usuario
+            // $field ... es el campo del modelo
+            // $table ... es el nombre de la tabla en la BD
+            // $fieldcode ... es el campo interno que quiero consultar
+            // $fieldtitle ... es el campo a mostar al usuario
+        // $this->addFilterAutocomplete($viewName, 'xIdvehicle_type', 'Vehículo - tipo', 'idvehicle_type', 'vehiculos', 'idvehicle_type', 'nombre');
+        
+        
+        
+        // Filtro periodo de fechas
+        // addFilterPeriod($viewName, $key, $label, $field)
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... es la etiqueta a mostrar al cliente
+            // $field ... es el campo sobre el que filtraremos
+        // $this->addFilterPeriod($viewName, 'porFechaAlta', 'Fecha de alta', 'fechaalta');
+        
+        // Filtro de fecha sin periodo
+        // addFilterDatePicker($viewName, $key, $label, $field)
+    }
+    
+}
---
+++ Model/Service_regular_period.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace FacturaScripts\Plugins\OpenServBus\Model; 
+
+use FacturaScripts\Core\Model\Base;
+
+class Service_regular_period extends Base\ModelClass {
+    use Base\ModelTrait;
+
+    public $idservice_regular_period;
+        
+    public $user_fecha;
+    public $user_nick;
+    public $fechaalta;
+    public $useralta;
+    public $fechamodificacion;
+    public $usermodificacion;
+    public $activo;
+    public $fechabaja;
+    public $userbaja;
+    public $motivobaja;
+    
+    public $idservice_regular;
+    public $fecha_desde;
+    public $fecha_hasta;
+    
+    public $hora_desde;
+    public $hora_hasta;
+    
+    public $salida_desde_nave_sn;
+    
+    public $observaciones;
+    
+    // función que inicializa algunos valores antes de la vista del controlador
+    public function clear() {
+        parent::clear();
+        
+        $this->activo = true; // Por defecto estará activo
+        $this->salida_desde_nave_sn = false;
+    }
+    
+    // función que devuelve el id principal
+    public static function primaryColumn(): string {
+        return 'idservice_regular_period';
+    }
+    
+    // función que devuelve el nombre de la tabla
+    public static function tableName(): string {
+        return 'service_regular_periods';
+    }
+
+    // Para realizar cambios en los datos antes de guardar por modificación
+    protected function saveUpdate(array $values = [])
+    {
+        $this->rellenarDatosModificacion();
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+
+        return parent::saveUpdate($values);
+    }
+
+    // Para realizar cambios en los datos antes de guardar por alta
+    protected function saveInsert(array $values = [])
+    {
+        // Creamos el nuevo id
+        if (empty($this->idservice_regular_period)) {
+            $this->idservice_regular_period = $this->newCode();
+        }
+
+        $this->rellenarDatosAlta();
+        $this->rellenarDatosModificacion();
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+
+        return parent::saveInsert($values);
+    }
+    
+    public function test() {
+        
+        if ($this->checkFechasPeriodo() == false){
+            return false;
+        }
+        
+        if ($this->checkHorasPeriodo() == false){
+            return false;
+        }
+        
+        /*
+        // Para evitar la inyección de sql
+        $utils = $this->toolBox()->utils();
+        $this->nombre = $utils->noHtml($this->nombre);
+        */
+        
+        return parent::test();
+    }
+
+
+    // ** ********************************** ** //
+    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
+    // ** ********************************** ** //
+    private function comprobarSiActivo()
+    {
+        $a_devolver = true;
+        
+        if ($this->activo == false) {
+            $this->fechabaja = $this->fechamodificacion;
+            $this->userbaja = $this->usermodificacion;
+            
+            if (empty($this->motivobaja)){
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
+            }
+        } else { // Por si se vuelve a poner Activo = true
+            $this->fechabaja = null;
+            $this->userbaja = null;
+            $this->motivobaja = null;
+        }
+        return $a_devolver;
+    }
+
+    private function rellenarDatosModificacion()
+    {
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+    }
+
+    private function rellenarDatosAlta()
+    {
+        $this->useralta = $this->user_nick; 
+        $this->fechaalta = $this->user_fecha; 
+    }
+
+    
+    
+    private function checkFechasPeriodo()
+    {
+        $a_devolver = true;
+        
+        // La fecha de inicio es obligatoria
+        if (empty($this->fecha_desde)) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('La fecha de inicio, debe de introducirla.');
+        }
+
+        // Si fecha hasta está introducida y fecha desde no está vacía y además es mayor que fecha hasta ... fallo
+        if (!empty($this->fecha_hasta)) 
+        {
+            if ( !empty($this->fecha_desde) and 
+                 $this->fecha_desde > $this->fecha_hasta ) 
+            {
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('La fecha de inicio, no puede ser mayor que la fecha de fin.');
+            }
+        }
+        return $a_devolver;
+    }
+    
+    private function checkHorasPeriodo()
+    {
+        $a_devolver = true;
+        
+        // La hora de inicio es obligatoria
+        if (empty($this->hora_desde)) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('La hora de inicio, debe de introducirla.');
+        }
+
+        // La hora de fin es obligatoria
+        if (empty($this->hora_hasta)) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('La hora fin, debe de introducirla.');
+        }
+
+        // Si fecha hasta está introducida y fecha desde no está vacía y además es mayor que fecha hasta ... fallo
+        if (!empty($this->hora_hasta)) 
+        {
+            if ( !empty($this->hora_desde) and 
+                 $this->hora_desde > $this->hora_hasta ) 
+            {
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('La hora de inicio, no puede ser mayor que la hora de fin.');
+            }
+        }
+        return $a_devolver;
+    }
+    
+}
