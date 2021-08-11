f6945cf83f0d10c0754a9ffbd4950d608e5f1c75
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-11 09:14:27 +0200

    Trabajando en el mantenimiento de itinerarios de servicios regulares SIN TERMINAR TODAVIA

---
+++ Controller/ListService_regular_itinerary.php
@@ -0,0 +1,86 @@
+<?php
+namespace FacturaScripts\Plugins\OpenServBus\Controller;
+
+use FacturaScripts\Core\Lib\ExtendedController\ListController;
+
+class ListService_regular_itinerary extends ListController {
+    // Para presentar la pantalla del controlador
+    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Empleados
+    public function getPageData(): array {
+        $pageData = parent::getPageData();
+        
+        $pageData['menu'] = 'OpenServBus';
+        $pageData['submenu'] = 'Servicios';
+        $pageData['title'] = 'Serv. regulares - Itinerarios';
+        
+        $pageData['icon'] = 'fas fa-road';
+        
+        return $pageData;
+    }
+    
+    protected function createViews() {
+        $this->createViewService_regular_itinerary();
+    }
+    
+    protected function createViewService_regular_itinerary($viewName = 'ListService_regular_itinerary')
+    {
+        $this->addView($viewName, 'Service_regular_itinerary');
+        
+        // Opciones de búsqueda rápida
+        // $this->addSearchFields($viewName, ['cod_servicio', 'nombre']);
+        
+        // Tipos de Ordenación
+            // Primer parámetro es la pestaña
+            // Segundo parámetro es los campos por los que ordena (array)
+            // Tercer parámetro es la etiqueta a poner
+            // Cuarto parámetro, si se rellena, le está diciendo cual es el order by por defecto, y además las opciones son
+               // 1 Orden ascendente
+               // 2 Orden descendente
+        $this->addOrderBy($viewName, ['idservice_regular', 'orden'], 'Por itinerario', 1);
+        $this->addOrderBy($viewName, ['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
+        
+        // Filtros
+        // Filtro checkBox por campo Activo ... addFilterCheckbox($viewName, $key, $label, $field);
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... la etiqueta a mostrar al usuario
+            // $field ... el campo del modelo sobre el que vamos a comprobar
+        // $this->addFilterCheckbox($viewName, 'conductor', 'Ver sólo conductores', 'driver_yn');
+        // $this->addFilterCheckbox($viewName, 'activo', 'Ver sólo los activos', 'activo');
+
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        // Sustituimos el filtro activo (checkBox) por el filtro activo (select)
+        $activo = [
+            ['code' => '1', 'description' => 'Activos = SI'],
+            ['code' => '0', 'description' => 'Activos = NO'],
+        ];
+        $this->addFilterSelect($viewName, 'soloActivos', 'Activos = TODOS', 'activo', $activo);        
+
+        // Filtro autoComplete ... addFilterAutocomplete($viewName, $key, $label, $field, $table, $fieldcode, $fieldtitle)
+        // Aunque lo vamos a hacer sobre la tabla empresa que normalmente tiene pocos registros
+        // este tipo de filtros está pensado para tablas como clientes, proveedores, etc que tengan muchos registros
+        // Para estas tablas no vamos a usar un filtro Select ... faltaría memoria al equipo para ello
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro, que puede ser el campo sobre el que quiero filtrar
+            // $label ...  parámetro es la etiqueta a mostrar al usuario
+            // $field ... es el campo del modelo
+            // $table ... es el nombre de la tabla en la BD
+            // $fieldcode ... es el campo interno que quiero consultar
+            // $fieldtitle ... es el campo a mostar al usuario
+        $this->addFilterAutocomplete($viewName, 'xIdservice_regular', 'Servicio regular', 'idservice_regular', 'service_regulars', 'idservice_regular', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'xIdstop', 'Parada', 'idstop', 'stops', 'idstop', 'nombre');
+        
+        
+        
+        // Filtro periodo de fechas
+        // addFilterPeriod($viewName, $key, $label, $field)
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... es la etiqueta a mostrar al cliente
+            // $field ... es el campo sobre el que filtraremos
+        // $this->addFilterPeriod($viewName, 'porFechaAlta', 'Fecha de alta', 'fechaalta');
+        
+        // Filtro de fecha sin periodo
+        // addFilterDatePicker($viewName, $key, $label, $field)
+    }
+    
+}
---
+++ Model/Service_regular_itinerary.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace FacturaScripts\Plugins\OpenServBus\Model; 
+
+use FacturaScripts\Core\Model\Base;
+
+class Service_regular_itinerary extends Base\ModelClass {
+    use Base\ModelTrait;
+
+    public $idservice_regular_itinerary;
+        
+    public $user_fecha;
+    public $user_nick;
+    public $fechaalta;
+    public $useralta;
+    public $fechamodificacion;
+    public $usermodificacion;
+    public $activo;
+    public $fechabaja;
+    public $userbaja;
+    public $motivobaja;
+    
+    public $idservice_regular;
+
+    public $orden;
+    public $idstop;
+    public $hora;
+    public $kms;
+    public $kms_vacios;
+    public $pasajeros_entradas;
+    public $pasajeros_salidas;
+    
+    public $observaciones;
+    
+    // función que inicializa algunos valores antes de la vista del controlador
+    public function clear() {
+        parent::clear();
+        
+        $this->activo = true; // Por defecto estará activo
+        $this->kms = 0;
+        $this->kms_vacios = false;
+        $this->pasajeros_entradas = 0;
+        $this->pasajeros_salidas = 0;
+    }
+    
+    // función que devuelve el id principal
+    public static function primaryColumn(): string {
+        return 'idservice_regular_itinerary';
+    }
+    
+    // función que devuelve el nombre de la tabla
+    public static function tableName(): string {
+        return 'service_regular_itineraries';
+    }
+
+    // Para realizar cambios en los datos antes de guardar por modificación
+    protected function saveUpdate(array $values = [])
+    {
+        $this->rellenarDatosModificacion();
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+
+        return parent::saveUpdate($values);
+    }
+
+    // Para realizar cambios en los datos antes de guardar por alta
+    protected function saveInsert(array $values = [])
+    {
+        // Creamos el nuevo id
+        if (empty($this->idservice_regular_itinerary)) {
+            $this->idservice_regular_itinerary = $this->newCode();
+        }
+
+        $this->rellenarDatosAlta();
+        $this->rellenarDatosModificacion();
+        
+        if ($this->comprobarSiActivo() == false){
+            return false;
+        }
+
+        return parent::saveInsert($values);
+    }
+    
+    public function test() {
+        
+        if ($this->checkOrden() == false){
+            return false;
+        }
+        
+        if ($this->checkHora() == false){
+            return false;
+        }
+        
+        if ($this->checkPasajeros() == false){
+            return false;
+        }
+        
+        // Para evitar la inyección de sql
+        $utils = $this->toolBox()->utils();
+        $this->observaciones = $utils->noHtml($this->observaciones);
+
+        
+        return parent::test();
+    }
+
+
+    // ** ********************************** ** //
+    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
+    // ** ********************************** ** //
+    private function comprobarSiActivo()
+    {
+        $a_devolver = true;
+        
+        if ($this->activo == false) {
+            $this->fechabaja = $this->fechamodificacion;
+            $this->userbaja = $this->usermodificacion;
+            
+            if (empty($this->motivobaja)){
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
+            }
+        } else { // Por si se vuelve a poner Activo = true
+            $this->fechabaja = null;
+            $this->userbaja = null;
+            $this->motivobaja = null;
+        }
+        return $a_devolver;
+    }
+
+    private function rellenarDatosModificacion()
+    {
+        $this->usermodificacion = $this->user_nick; 
+        $this->fechamodificacion = $this->user_fecha; 
+    }
+
+    private function rellenarDatosAlta()
+    {
+        $this->useralta = $this->user_nick; 
+        $this->fechaalta = $this->user_fecha; 
+    }
+
+    private function checkOrden()
+    {
+        $a_devolver = true;
+        if (empty($this->orden)) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('Falta el orden de la línea. Es un valor numérico para ordenar los itinerarios.');
+        }
+        return $a_devolver;
+    }
+    
+    private function checkHora()
+    {
+        $a_devolver = true;
+        if (empty($this->hora)) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('Falta el hora en la que debe de estar en la parada.');
+        }
+        return $a_devolver;
+    }
+    
+    private function checkPasajeros()
+    {
+        $a_devolver = true;
+        if ( empty($this->pasajeros_entradas) ||
+             empty($this->pasajeros_salidas) ) 
+        {
+            $a_devolver = false;
+            $this->toolBox()->i18nLog()->error('Debe de asignar la cantidad de pasajeros a recoger/dejar.');
+        }
+        return $a_devolver;
+    }
+    
+}
