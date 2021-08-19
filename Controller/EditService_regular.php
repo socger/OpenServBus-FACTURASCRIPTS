3b9fb812da8da01387227e2f90f39118802fe6f4
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-19 10:00:12 +0200

    SERVICIOS REGULARES ... Creamos pestaña COMBINACIONES para poder ver las combinaciones en las que se encuentra el servicio.

--- Controller/EditService_regular.php
+++ Controller/EditService_regular.php
@@ -31,9 +31,39 @@ class EditService_regular extends EditController {
         
         $this->createViewService_regular_period();
         $this->createViewService_regular_itinerary();
+        $this->createViewService_regular_combination_serv();
         
         $this->setTabsPosition('top'); // Las posiciones de las pestañas pueden ser left, top, down
     }
+    
+    protected function createViewService_regular_combination_serv($model = 'Service_regular_combination_serv')
+    {
+        // $this->addListView($viewName, $modelName, $viewTitle, $viewIcon)
+        // $viewName: el identificador o nombre interno de esta pestaña o sección. Por ejemplo: ListProducto.
+        // $modelName: el nombre del modelo que usará este listado. Por ejemplo: Producto.
+        // $viewTitle: el título de la pestaña o sección. Será tarducido. Por ejemplo: products.
+        // $viewIcon: (opcional) el icono a utilizar. Por ejemplo: fas fa-search.
+        $this->addListView('List' . $model, $model, 'Combinaciones', 'fas fa-briefcase');    
+        
+
+        $this->views['List' . $model]->addOrderBy(['idservice_regular_combination', 'idservice_regular'], 'Nombre', 1);
+        $this->views['List' . $model]->addOrderBy(['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
+
+        
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        // Sustituimos el filtro activo (checkBox) por el filtro activo (select)
+        $activo = [
+            ['code' => '1', 'description' => 'Activos = SI'],
+            ['code' => '0', 'description' => 'Activos = NO'],
+        ];
+        $this->views['List' . $model]->addFilterSelect('soloActivos', 'Activos = TODOS', 'activo', $activo);
+
+        
+        $this->views['List' . $model]->addFilterAutocomplete('xIdDriver', 'driver', 'iddriver', 'drivers', 'iddriver', 'nombre');
+        $this->views['List' . $model]->addFilterAutocomplete('xIdVehicle', 'vehicle', 'idvehicle', 'vehicles', 'idvehicle', 'nombre');
+        $this->views['List' . $model]->addFilterAutocomplete('xIdVehicle', 'vehicle', 'idvehicle', 'vehicles', 'idvehicle', 'nombre');
+        $this->views['List' . $model]->addFilterAutocomplete('xIdservice_regular_combination', 'combination-service', 'idservice_regular_combination', 'service_regular_combinations', 'idservice_regular_combination', 'nombre');
+}
     
     protected function createViewService_regular_itinerary($model = 'Service_regular_itinerary')
     {
@@ -96,6 +126,12 @@ class EditService_regular extends EditController {
     // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
     protected function loadData($viewName, $view) {
         switch ($viewName) {
+            case 'ListService_regular_combination_serv':
+                $idservice_regular = $this->getViewModelValue('EditService_regular', 'idservice_regular');
+                $where = [new DatabaseWhere('idservice_regular', $idservice_regular)];
+                $view->loadData('', $where);
+                break;
+            
             case 'ListService_regular_itinerary':
                 $idservice_regular = $this->getViewModelValue('EditService_regular', 'idservice_regular');
                 $where = [new DatabaseWhere('idservice_regular', $idservice_regular)];
