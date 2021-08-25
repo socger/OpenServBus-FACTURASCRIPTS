b25ef01e3fb7c077ad8171c0118137599f92c1b8
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-25 13:34:28 +0200

    Creado el mantenimiento de servicios discrecionales.

---
+++ Controller/EditService.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace FacturaScripts\Plugins\OpenServBus\Controller;
+
+ use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+use FacturaScripts\Core\Lib\ExtendedController\EditController;
+
+class EditService extends EditController {
+    
+    public function getModelClassName() {
+        return 'Service';
+    }
+    
+    // Para presentar la pantalla del controlador
+    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
+    public function getPageData(): array {
+        $pageData = parent::getPageData();
+
+        $pagedata['showonmenu'] = false;
+        $pageData['menu'] = 'OpenServBus';
+        $pageData['title'] = 'Servicio discrecional';
+
+        $pageData['icon'] = 'fas fa-book-reader';
+        
+
+        return $pageData;
+    }
+    
+    protected function createViews() {
+        parent::createViews();
+        
+        $this->createViewService_itinerary();
+        $this->setTabsPosition('top'); // Las posiciones de las pestañas pueden ser left, top, down
+    }
+    
+    protected function createViewService_itinerary($model = 'Service')
+    {
+        // $this->addListView($viewName, $modelName, $viewTitle, $viewIcon)
+        // $viewName: el identificador o nombre interno de esta pestaña o sección. Por ejemplo: ListProducto.
+        // $modelName: el nombre del modelo que usará este listado. Por ejemplo: Producto.
+        // $viewTitle: el título de la pestaña o sección. Será tarducido. Por ejemplo: products.
+        // $viewIcon: (opcional) el icono a utilizar. Por ejemplo: fas fa-search.
+        $this->addListView('List' . $model, $model, 'Itinerarios', 'fas fa-road');    
+        
+        $this->views['List' . $model]->addOrderBy(['idservice', 'orden'], 'Por itinerario', 1);
+        $this->views['List' . $model]->addOrderBy(['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
+        
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        // Sustituimos el filtro activo (checkBox) por el filtro activo (select)
+        $activo = [
+            ['code' => '1', 'description' => 'Activos = SI'],
+            ['code' => '0', 'description' => 'Activos = NO'],
+        ];
+        $this->views['List' . $model]->addFilterSelect('soloActivos', 'Activos = TODOS', 'activo', $activo);
+
+        $this->views['List' . $model]->addFilterAutocomplete('xIdservice', 'Servicio discrecional', 'idservice', 'services', 'idservice', 'nombre');
+        $this->views['List' . $model]->addFilterAutocomplete('xIdstop', 'Parada', 'idstop', 'stops', 'idstop', 'nombre');
+    }
+    
+    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
+    protected function loadData($viewName, $view) {
+        switch ($viewName) {
+            case 'ListService_itinerary':
+                $idservice = $this->getViewModelValue('EditService', 'idservice');
+                $where = [new DatabaseWhere('idservice', $idservice)];
+                $view->loadData('', $where);
+                break;
+            
+            // Pestaña con el mismo nombre que este controlador EditXxxxx
+            case 'EditService': 
+                parent::loadData($viewName, $view);
+                
+                // Guardamos que usuario pulsará guardar
+                $this->views[$viewName]->model->user_nick = $this->user->nick;
+
+                // Guardamos cuando el usuario pulsará guardar
+             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
+                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
+                
+                $this->prepararFechasParaVista($viewName);
+                $this->prepararHorasParaVista($viewName);
+
+                break;
+        }
+    }
+
+
+    // ** *************************************** ** //
+    // ** FUNCIONES CREADAS PARA ESTE CONTROLADOR ** //
+    // ** *************************************** ** //
+    private function prepararFechasParaVista($viewName)
+    {
+        if (!empty($this->views[$viewName]->model->fecha_desde)){
+            $this->views[$viewName]->model->inicio_dia = date("Y-m-d", strtotime($this->views[$viewName]->model->fecha_desde));
+        } else {
+            $this->views[$viewName]->model->inicio_dia = null;
+        }
+
+        if (!empty($this->views[$viewName]->model->fecha_hasta)){
+            $this->views[$viewName]->model->fin_dia = date("Y-m-d", strtotime($this->views[$viewName]->model->fecha_hasta));
+        } else {
+            $this->views[$viewName]->model->fin_dia = null;
+        }
+    }
+
+    private function prepararHorasParaVista($viewName)
+    {
+        if (!empty($this->views[$viewName]->model->hora_desde)){
+            $this->views[$viewName]->model->inicio_hora = date("H:i:s", strtotime($this->views[$viewName]->model->hora_desde));
+        } else {
+            $this->views[$viewName]->model->inicio_hora = null;
+        }
+
+        if (!empty($this->views[$viewName]->model->hora_hasta)){
+            $this->views[$viewName]->model->fin_hora = date("H:i:s", strtotime($this->views[$viewName]->model->hora_hasta));
+        } else {
+            $this->views[$viewName]->model->fin_hora = null;
+        }
+    }
+    
+}
---
+++ Controller/ListService.php
@@ -0,0 +1,96 @@
+<?php
+namespace FacturaScripts\Plugins\OpenServBus\Controller;
+
+use FacturaScripts\Core\Lib\ExtendedController\ListController;
+
+class ListService extends ListController {
+    
+    // Para presentar la pantalla del controlador
+    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Empleados
+    public function getPageData(): array {
+        $pageData = parent::getPageData();
+        
+        $pageData['menu'] = 'OpenServBus';
+        $pageData['submenu'] = 'Serv. discrecionales';
+        $pageData['title'] = 'Servicios discrecionales';
+        
+        $pageData['icon'] = 'fas fa-book-reader';
+
+        return $pageData;
+    }
+    
+    protected function createViews() {
+        $this->createViewService();
+    }
+    
+    protected function createViewService($viewName = 'ListService')
+    {
+        $this->addView($viewName, 'Service');
+        
+        // Opciones de búsqueda rápida
+        $this->addSearchFields($viewName, ['idservicio', 'nombre']);
+        
+        // Tipos de Ordenación
+            // Primer parámetro es la pestaña
+            // Segundo parámetro es los campos por los que ordena (array)
+            // Tercer parámetro es la etiqueta a poner
+            // Cuarto parámetro, si se rellena, le está diciendo cual es el order by por defecto, y además las opciones son
+               // 1 Orden ascendente
+               // 2 Orden descendente
+        $this->addOrderBy($viewName, ['nombre'], 'Nombre', 1);
+        $this->addOrderBy($viewName, ['idservicio'], 'Código');
+        $this->addOrderBy($viewName, ['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
+        
+        // Filtros
+        // Filtro checkBox por campo Activo ... addFilterCheckbox($viewName, $key, $label, $field);
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... la etiqueta a mostrar al usuario
+            // $field ... el campo del modelo sobre el que vamos a comprobar
+        // $this->addFilterCheckbox($viewName, 'conductor', 'Ver sólo conductores', 'driver_yn');
+        // $this->addFilterCheckbox($viewName, 'activo', 'Ver sólo los activos', 'activo');
+
+        // Filtro de TIPO SELECT para filtrar por registros activos (SI, NO, o TODOS)
+        $activo = [
+            ['code' => '1', 'description' => 'Activos = SI'],
+            ['code' => '0', 'description' => 'Activos = NO'],
+        ];
+        $this->addFilterSelect($viewName, 'soloActivos', 'Activos = TODOS', 'activo', $activo);        
+
+        // Filtro de TIPO SELECT para filtrar por SERVICIOS REGULARES FACTURABLES (SI, NO, o TODOS)
+        $crearFtraSN = [
+            ['code' => '1', 'description' => 'Crear ftra. = SI'],
+            ['code' => '0', 'description' => 'Crear ftra. = NO'],
+        ];
+        $this->addFilterSelect($viewName, 'crearFtra', 'Crear ftra. = TODOS', 'facturar_SN', $crearFtraSN);        
+
+        // Filtro autoComplete ... addFilterAutocomplete($viewName, $key, $label, $field, $table, $fieldcode, $fieldtitle)
+        // Aunque lo vamos a hacer sobre la tabla empresa que normalmente tiene pocos registros
+        // este tipo de filtros está pensado para tablas como clientes, proveedores, etc que tengan muchos registros
+        // Para estas tablas no vamos a usar un filtro Select ... faltaría memoria al equipo para ello
+            // $viewName ... nombre del controlador
+            // $key ... es el nombre que le ponemos al filtro, que puede ser el campo sobre el que quiero filtrar
+            // $label ...  parámetro es la etiqueta a mostrar al usuario
+            // $field ... es el campo del modelo
+            // $table ... es el nombre de la tabla en la BD
+            // $fieldcode ... es el campo interno que quiero consultar
+            // $fieldtitle ... es el campo a mostar al usuario
+        $this->addFilterAutocomplete($viewName, 'xCodCliente', 'Cliente', 'codcliente', 'clientes', 'codcliente', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'xIdvehicle_type', 'Vehículo - tipo', 'idvehicle_type', 'vehiculos', 'idvehicle_type', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'xIdhelper', 'Monitor/a', 'idhelper', 'helpers', 'idhelper', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'xIdservice_type', 'Servicio - tipo', 'idservice_type', 'service_types', 'idservice_type', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'xIdempresa', 'Empresa', 'idempresa', 'empresas', 'idempresa', 'nombre');
+        
+        // Filtro periodo de fechas
+        // addFilterPeriod($viewName, $key, $label, $field)
+            // $key ... es el nombre que le ponemos al filtro
+            // $label ... es la etiqueta a mostrar al cliente
+            // $field ... es el campo sobre el que filtraremos
+        $this->addFilterPeriod($viewName, 'porFechaInicio', 'F.inicio', 'fecha_desde');
+        $this->addFilterPeriod($viewName, 'porFechaFin', 'F.fin', 'fecha_hasta');
+        
+        // Filtro de fecha sin periodo
+        // addFilterDatePicker($viewName, $key, $label, $field)
+    }
+    
+}
