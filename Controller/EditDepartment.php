51034b72f87ca41b0f8dec0cca5669bb6016141e
Daniel Fernández <daniel89fg@gmail.com>
2022-11-28 11:22:42 +0100

    Ordenación modelo Department

--- Controller/EditDepartment.php
+++ Controller/EditDepartment.php
@@ -4,42 +4,20 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
-class EditDepartment extends EditController {
-    
-    public function getModelClassName() {
+class EditDepartment extends EditController
+{
+    public function getModelClassName(): string
+    {
         return 'Department';
     }
-    
-    // Para presentar la pantalla del controlador
-    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
-    public function getPageData(): array {
+
+    public function getPageData(): array
+    {
         $pageData = parent::getPageData();
-        
-        $pagedata['showonmenu'] = false;
+        $pageData['showonmenu'] = false;
         $pageData['menu'] = 'OpenServBus';
         $pageData['title'] = 'Departamento';
-        
         $pageData['icon'] = 'fas fa-book-reader';
-
         return $pageData;
     }
-    
-    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
-    protected function loadData($viewName, $view) {
-        switch ($viewName) {
-
-            // Pestaña con el mismo nombre que este controlador EditXxxxx
-            case 'EditDepartment': 
-                parent::loadData($viewName, $view);
-                
-                // Guardamos que usuario y cuando pulsará guardar
-                $this->views[$viewName]->model->user_nick = $this->user->nick;
-
-             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
-                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
-                
-                break;
-        }
-    }
-    
 }
