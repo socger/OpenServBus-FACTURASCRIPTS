d4f84ff8e633022cb59a8e7a04840c6f44ffab9c
Daniel Fernández <daniel89fg@gmail.com>
2022-11-28 11:08:32 +0100

    Ordenado modelo Garage.

--- Controller/EditGarage.php
+++ Controller/EditGarage.php
@@ -4,64 +4,20 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
-class EditGarage extends EditController {
-    
-    public $seModifica;  
-    
-    public function getModelClassName() {
+class EditGarage extends EditController
+{
+    public function getModelClassName(): string
+    {
         return 'Garage';
     }
-    
-    // Para presentar la pantalla del controlador
-    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
-    public function getPageData(): array {
+
+    public function getPageData(): array
+    {
         $pageData = parent::getPageData();
-        
-        $pagedata['showonmenu'] = false;
+        $pageData['showonmenu'] = false;
         $pageData['menu'] = 'OpenServBus';
         $pageData['title'] = 'Cochera';
-        
         $pageData['icon'] = 'fas fa-warehouse';
-
         return $pageData;
-    }
-    
-    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
-    protected function loadData($viewName, $view) {
-        switch ($viewName) {
-
-            // Pestaña con el mismo nombre que este controlador EditXxxxx
-            case 'EditGarage': 
-                parent::loadData($viewName, $view);
-                
-                // Asigna por defecto el usuario que da el alta, con el usuario que se ha registrado en la web
-                // Pero sólo si estamos en modo Insert/Append, en modo Edit no lo hace
-
-//                if(!$this->views[$viewName]->model->exists()) { 
-//                    // Estamos en un alta
-//                    $this->views[$viewName]->model->useralta = $this->user->nick; // Rellenamos useralta
-//                    $this->views[$viewName]->model->fechaalta = date('d-m-Y');
-//                } else {
-//                    // Estamos en una modificación
-//                    // $this->views[$viewName]->model->usermodificacion = $this->user->nick;
-//                    // $this->views[$viewName]->model->fechamodificacion = date('d-m-Y');
-//                    $this->seModifica = 'SI';
-//                }
-//
-                // Guardamos que usuario y cuando pulsará guardar
-                $this->views[$viewName]->model->user_nick = $this->user->nick;
-
-             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
-                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
-                
-                break;
-        }
-    }
-    
-    protected function saveData($viewName, $view) {
-        if ($this->seModifica == 'SI') {
-            $this->views[$viewName]->model->usermodificacion = $this->user->nick;
-            $this->views[$viewName]->model->fechamodificacion = date('d-m-Y');
-        };
     }
 }
--- Model/Garage.php
+++ Model/Garage.php
@@ -1,146 +1,108 @@
 <?php
-namespace FacturaScripts\Plugins\OpenServBus\Model; 
+
+namespace FacturaScripts\Plugins\OpenServBus\Model;
 
 use FacturaScripts\Core\Model\Base;
+use FacturaScripts\Core\Session;
 
-class Garage extends Base\ModelClass {
+class Garage extends Base\ModelClass
+{
     use Base\ModelTrait;
 
-    public $idgarage;
-    
-    public $user_fecha;
-    public $user_nick;
-    public $fechaalta;
-    public $useralta;
-    public $fechamodificacion;
-    public $usermodificacion;
+    /** @var bool */
     public $activo;
+
+    /** @var string */
+    public $apartado;
+
+    /** @var string */
+    public $ciudad;
+
+    /** @var string */
+    public $codpais;
+
+    /** @var string */
+    public $codpostal;
+
+    /** @var string */
+    public $direccion;
+
+    /** @var string */
+    public $email;
+
+    /** @var string */
+    public $fax;
+
+    /** @var string */
+    public $fechaalta;
+
+    /** @var string */
     public $fechabaja;
-    public $userbaja;
+
+    /** @var string */
+    public $fechamodificacion;
+
+    /** @var int */
+    public $idempresa;
+
+    /** @var int */
+    public $idgarage;
+
+    /** @var string */
     public $motivobaja;
 
-    public $idempresa;
+    /** @var string */
     public $nombre;
-    public $ciudad;
-    public $provincia;
-    public $codpais;
-    public $codpostal;
-    public $apartado;
-    public $direccion;
-    public $telefono1;
-    public $telefono2;
-    public $fax;
-    public $email;
-    public $web;
-    
+
+    /** @var string */
     public $observaciones;
-    
-    // función que inicializa algunos valores antes de la vista del controlador
-    public function clear() {
+
+    /** @var string */
+    public $provincia;
+
+    /** @var string */
+    public $telefono1;
+
+    /** @var string */
+    public $telefono2;
+
+    /** @var string */
+    public $useralta;
+
+    /** @var string */
+    public $userbaja;
+
+    /** @var string */
+    public $usermodificacion;
+
+    /** @var string */
+    public $web;
+
+    public function clear()
+    {
         parent::clear();
-        
-     // $this->fechamodificacion = date('d-m-Y'); // Lo quitamos porque lo vamos a rellenar, por estética, en el saveInsert
-     // $this->fechaalta = date('d-m-Y'); // Rellena automáticamente con la fecha de hoy a el field fechaalta
+        $this->activo = true;
         $this->codpais = $this->toolBox()->appSettings()->get('default', 'codpais');
-        $this->activo = true; // Por defecto estará activo
+        $this->fechaalta = date(static::DATETIME_STYLE);
+        $this->useralta = Session::get('user')->nick ?? null;
     }
-    
-    // función que devuelve el id principal
-    public static function primaryColumn(): string {
+
+    public static function primaryColumn(): string
+    {
         return 'idgarage';
     }
-    
-    // función que devuelve el nombre de la tabla
-    public static function tableName(): string {
+
+    public static function tableName(): string
+    {
         return 'garages';
     }
 
-    // Para realizar cambios en los datos antes de guardar por modificación
-    protected function saveUpdate(array $values = [])
-    {
-        $this->rellenarDatosModificacion();
-        
-        if ($this->comprobarSiActivo() == false){
-            return false;
-        }
-        
-        return parent::saveUpdate($values);
-    }
-
-    // Para realizar cambios en los datos antes de guardar por alta
-    protected function saveInsert(array $values = [])
-    {
-        // Creamos el nuevo id
-        if (empty($this->idgarage)) {
-            $this->idgarage = $this->newCode();
-        }
-
-        $this->rellenarDatosAlta();
-        $this->rellenarDatosModificacion();
-        
-        // echo $this->active;
-        // sleep(60);
-        
-        if ($this->comprobarSiActivo() == false){
-            return false;
-        }
-        
-        return parent::saveInsert($values);
-    }
-    
-    public function test()
+    public function test(): bool
     {
         if (empty($this->idempresa)) {
             $this->idempresa = $this->toolBox()->appSettings()->get('default', 'idempresa');
         }
 
-        $this->evitarInyeccionSQL();
-        return parent::test();
-    }
-
-    public function url(string $type = 'auto', string $list = 'ListHelper'): string
-    {
-        return parent::url($type, $list . '?activetab=List');
-    }
-
-
-    // ** ********************************** ** //
-    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
-    // ** ********************************** ** //
-    private function comprobarSiActivo()
-    {
-        $a_devolver = true;
-        
-        if ($this->activo == false) {
-            $this->fechabaja = $this->fechamodificacion;
-            $this->userbaja = $this->usermodificacion;
-            
-            if (empty($this->motivobaja)){
-                $a_devolver = false;
-                $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
-            }
-        } else { // Por si se vuelve a poner Activo = true
-            $this->fechabaja = null;
-            $this->userbaja = null;
-            $this->motivobaja = null;
-        }
-        return $a_devolver;
-    }
-
-    private function rellenarDatosModificacion()
-    {
-        $this->usermodificacion = $this->user_nick; 
-        $this->fechamodificacion = $this->user_fecha; 
-    }
-
-    private function rellenarDatosAlta()
-    {
-        $this->useralta = $this->user_nick; 
-        $this->fechaalta = $this->user_fecha; 
-    }
-	
-    private function evitarInyeccionSQL()
-    {
         $utils = $this->toolBox()->utils();
         $this->nombre = $utils->noHtml($this->nombre);
         $this->ciudad = $utils->noHtml($this->ciudad);
@@ -156,6 +118,49 @@ class Garage extends Base\ModelClass {
         $this->web = $utils->noHtml($this->web);
         $this->observaciones = $utils->noHtml($this->observaciones);
         $this->motivobaja = $utils->noHtml($this->motivobaja);
+        return parent::test();
     }
-	
+
+    public function url(string $type = 'auto', string $list = 'ListHelper'): string
+    {
+        return parent::url($type, $list . '?activetab=List');
+    }
+
+    protected function comprobarSiActivo(): bool
+    {
+        $a_devolver = true;
+        if ($this->activo === false) {
+            $this->fechabaja = $this->fechamodificacion;
+            $this->userbaja = $this->usermodificacion;
+
+            if (empty($this->motivobaja)) {
+                $a_devolver = false;
+                $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
+            }
+        } else { // Por si se vuelve a poner Activo = true
+            $this->fechabaja = null;
+            $this->userbaja = null;
+            $this->motivobaja = null;
+        }
+        return $a_devolver;
+    }
+
+    protected function saveInsert(array $values = []): bool
+    {
+        if ($this->comprobarSiActivo() === false) {
+            return false;
+        }
+
+        return parent::saveInsert($values);
+    }
+
+    protected function saveUpdate(array $values = []): bool
+    {
+        if ($this->comprobarSiActivo() === false) {
+            return false;
+        }
+        $this->usermodificacion = Session::get('user')->nick ?? null;
+        $this->fechamodificacion = date(static::DATETIME_STYLE);
+        return parent::saveUpdate($values);
+    }
 }
