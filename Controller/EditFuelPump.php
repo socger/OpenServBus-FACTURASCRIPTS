aff7124c0ed2b26fefd3297faaac04083c72bdcd
Daniel Fernández <daniel89fg@gmail.com>
2022-11-29 10:57:07 +0100

    Ordenar modelo FuelPump.

--- Controller/EditFuelPump.php
+++ Controller/EditFuelPump.php
@@ -4,42 +4,20 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
-class EditFuelPump extends EditController {
-    
-    public function getModelClassName() {
+class EditFuelPump extends EditController
+{
+    public function getModelClassName(): string
+    {
         return 'FuelPump';
     }
-    
-    // Para presentar la pantalla del controlador
-    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
-    public function getPageData(): array {
+
+    public function getPageData(): array
+    {
         $pageData = parent::getPageData();
-        
-        $pagedata['showonmenu'] = false;
+        $pageData['showonmenu'] = false;
         $pageData['menu'] = 'OpenServBus';
         $pageData['title'] = 'Surtidor interno';
-        
         $pageData['icon'] = 'fas fa-thumbtack';
-
         return $pageData;
     }
-    
-    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
-    protected function loadData($viewName, $view) {
-        switch ($viewName) {
-
-            // Pestaña con el mismo nombre que este controlador EditXxxxx
-            case 'EditFuelPump':
-                parent::loadData($viewName, $view);
-                
-                // Guardamos que usuario y cuando pulsará guardar
-                $this->views[$viewName]->model->user_nick = $this->user->nick;
-
-             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
-                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
-                
-                break;
-        }
-    }
-    
 }
