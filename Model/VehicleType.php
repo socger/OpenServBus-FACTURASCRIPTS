01b09f387131ee930d8a0593e112aee8ab3f94d4
Daniel Fernández <daniel89fg@gmail.com>
2022-11-30 12:24:49 +0100

    Ordenar modelo VehicleType

--- Model/VehicleType.php
+++ Model/VehicleType.php
@@ -1,78 +1,77 @@
 <?php
 
-namespace FacturaScripts\Plugins\OpenServBus\Model; 
+namespace FacturaScripts\Plugins\OpenServBus\Model;
 
 use FacturaScripts\Core\Model\Base;
+use FacturaScripts\Core\Session;
 
-class VehicleType extends Base\ModelClass {
+class VehicleType extends Base\ModelClass
+{
     use Base\ModelTrait;
 
-    public $idvehicle_type;
-        
-    public $user_fecha;
-    public $user_nick;
-    public $fechaalta;
-    public $useralta;
-    public $fechamodificacion;
-    public $usermodificacion;
+    /** @var bool */
     public $activo;
+
+    /** @var string */
+    public $fechaalta;
+
+    /** @var string */
     public $fechabaja;
-    public $userbaja;
+
+    /** @var string */
+    public $fechamodificacion;
+
+    /** @var int */
+    public $idvehicle_type;
+
+    /** @var string */
     public $motivobaja;
 
+    /** @var string */
     public $nombre;
+
+    /** @var string */
     public $observaciones;
-    
-    // función que inicializa algunos valores antes de la vista del controlador
-    public function clear() {
+
+    /** @var string */
+    public $useralta;
+
+    /** @var string */
+    public $userbaja;
+
+    /** @var string */
+    public $usermodificacion;
+
+    public function clear()
+    {
         parent::clear();
-        
-        $this->activo = true; // Por defecto estará activo
+        $this->activo = true;
+        $this->fechaalta = date(static::DATETIME_STYLE);
+        $this->useralta = Session::get('user')->nick ?? null;
     }
-    
+
     // función que devuelve el id principal
-    public static function primaryColumn(): string {
+    public static function primaryColumn(): string
+    {
         return 'idvehicle_type';
     }
-    
+
     // función que devuelve el nombre de la tabla
-    public static function tableName(): string {
+    public static function tableName(): string
+    {
         return 'vehicle_types';
     }
 
-    // Para realizar cambios en los datos antes de guardar por modificación
-    protected function saveUpdate(array $values = [])
+    public function test(): bool
     {
-        $this->rellenarDatosModificacion();
-        
-        if ($this->comprobarSiActivo() == false){
+        if ($this->comprobarSiActivo() === false) {
             return false;
         }
-        
-        return parent::saveUpdate($values);
-    }
 
-    // Para realizar cambios en los datos antes de guardar por alta
-    protected function saveInsert(array $values = [])
-    {
-        // Creamos el nuevo id
-        if (empty($this->idvehicle_type)) {
-            $this->idvehicle_type = $this->newCode();
-        }
-
-        $this->rellenarDatosAlta();
-        $this->rellenarDatosModificacion();
-        
-        if ($this->comprobarSiActivo() == false){
-            return false;
-        }
-        
-        return parent::saveInsert($values);
-    }
-    
-    public function test()
-    {
-        $this->evitarInyeccionSQL();
+        $utils = $this->toolBox()->utils();
+        $this->nombre = $utils->noHtml($this->nombre);
+        $this->observaciones = $utils->noHtml($this->observaciones);
+        $this->motivobaja = $utils->noHtml($this->motivobaja);
         return parent::test();
     }
 
@@ -81,23 +80,19 @@ class VehicleType extends Base\ModelClass {
         return parent::url($type, $list . '?activetab=List');
     }
 
-
-    // ** ********************************** ** //
-    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
-    // ** ********************************** ** //
-    private function comprobarSiActivo()
+    protected function comprobarSiActivo(): bool
     {
         $a_devolver = true;
-        
-        if ($this->activo == false) {
+        if ($this->activo === false) {
             $this->fechabaja = $this->fechamodificacion;
             $this->userbaja = $this->usermodificacion;
-            
-            if (empty($this->motivobaja)){
+
+            if (empty($this->motivobaja)) {
                 $a_devolver = false;
                 $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
             }
-        } else { // Por si se vuelve a poner Activo = true
+        } else {
+            // Por si se vuelve a poner Activo = true
             $this->fechabaja = null;
             $this->userbaja = null;
             $this->motivobaja = null;
@@ -105,23 +100,10 @@ class VehicleType extends Base\ModelClass {
         return $a_devolver;
     }
 
-    private function rellenarDatosModificacion()
+    protected function saveUpdate(array $values = []): bool
     {
-        $this->usermodificacion = $this->user_nick; 
-        $this->fechamodificacion = $this->user_fecha; 
-    }
-
-    private function rellenarDatosAlta()
-    {
-        $this->useralta = $this->user_nick; 
-        $this->fechaalta = $this->user_fecha; 
-    }
-	
-    private function evitarInyeccionSQL()
-    {
-        $utils = $this->toolBox()->utils();
-        $this->nombre = $utils->noHtml($this->nombre);
-        $this->observaciones = $utils->noHtml($this->observaciones);
-        $this->motivobaja = $utils->noHtml($this->motivobaja);
+        $this->usermodificacion = Session::get('user')->nick ?? null;
+        $this->fechamodificacion = date(static::DATETIME_STYLE);
+        return parent::saveUpdate($values);
     }
 }
