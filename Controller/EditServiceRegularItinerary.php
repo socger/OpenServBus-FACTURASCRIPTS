e1a7bcdd53ba839304a79bcc86d3dff6ac2be5bf
Daniel Fernández <daniel89fg@gmail.com>
2022-11-30 11:30:20 +0100

    Ordenar modelo ServiceRegularItinerary

--- Controller/EditServiceRegularItinerary.php
+++ Controller/EditServiceRegularItinerary.php
@@ -4,57 +4,20 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
-class EditServiceRegularItinerary extends EditController {
-    
-    public function getModelClassName() {
+class EditServiceRegularItinerary extends EditController
+{
+    public function getModelClassName(): string
+    {
         return 'ServiceRegularItinerary';
     }
-    
-    // Para presentar la pantalla del controlador
-    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
-    public function getPageData(): array {
+
+    public function getPageData(): array
+    {
         $pageData = parent::getPageData();
-        
-        $pagedata['showonmenu'] = false;
+        $pageData['showonmenu'] = false;
         $pageData['menu'] = 'OpenServBus';
         $pageData['title'] = 'Serv. regular - Itinerario';
-        
         $pageData['icon'] = 'fas fa-road';
-        
         return $pageData;
     }
-    
-    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
-    protected function loadData($viewName, $view) {
-        switch ($viewName) {
-
-            // Pestaña con el mismo nombre que este controlador EditXxxxx
-            case 'EditServiceRegularItinerary':
-                parent::loadData($viewName, $view);
-                
-                // Guardamos que usuario y cuando pulsará guardar
-                $this->views[$viewName]->model->user_nick = $this->user->nick;
-
-             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
-                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
-
-                $this->prepararHoraParaVista($viewName);
-
-                break;
-        }
-    }
-
-
-    // ** *************************************** ** //
-    // ** FUNCIONES CREADAS PARA ESTE CONTROLADOR ** //
-    // ** *************************************** ** //
-    private function prepararHoraParaVista($viewName)
-    {
-        if (!empty($this->views[$viewName]->model->hora)){
-            $this->views[$viewName]->model->inicio_hora = date("H:i:s", strtotime($this->views[$viewName]->model->hora));
-        } else {
-            $this->views[$viewName]->model->inicio_hora = null;
-        }
-    }
-
 }
