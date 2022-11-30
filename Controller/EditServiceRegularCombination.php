b4c4bcbc0ae89d12f51e650f6a5781879fc06fd6
Daniel Fernández <daniel89fg@gmail.com>
2022-11-30 10:44:02 +0100

    Ordenar modelo ServiceRegularCombination

--- Controller/EditServiceRegularCombination.php
+++ Controller/EditServiceRegularCombination.php
@@ -5,38 +5,36 @@ namespace FacturaScripts\Plugins\OpenServBus\Controller;
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
-class EditServiceRegularCombination extends EditController {
-    
-    public function getModelClassName() {
+class EditServiceRegularCombination extends EditController
+{
+    public function getModelClassName(): string
+    {
         return 'ServiceRegularCombination';
     }
-    
-    // Para presentar la pantalla del controlador
-    // Estará en el el menú principal bajo \\OpenServBus\Archivos\Cocheras
-    public function getPageData(): array {
+
+    public function getPageData(): array
+    {
         $pageData = parent::getPageData();
-        
-        $pagedata['showonmenu'] = false;
+        $pageData['showonmenu'] = false;
         $pageData['menu'] = 'OpenServBus';
         $pageData['title'] = 'Serv. regulares - Combinación';
-        
         $pageData['icon'] = 'fas fa-briefcase';
-
         return $pageData;
     }
-    
-    protected function createViews() {
+
+    protected function createViews()
+    {
         parent::createViews();
         $this->createViewServiceRegularCombination_serv();
-        $this->setTabsPosition('top'); // Las posiciones de las pestañas pueden ser left, top, down
+        $this->setTabsPosition('top');
     }
-    
+
     protected function createViewServiceRegularCombination_serv($viewName = 'ListServiceRegularCombinationServ')
     {
         $this->addListView($viewName, 'ServiceRegularCombinationServ', 'Servicios', 'fas fa-cogs');
         $this->views[$viewName]->addOrderBy(['idservice_regular_combination', 'idservice_regular'], 'Nombre', 1);
         $this->views[$viewName]->addOrderBy(['fechaalta', 'fechamodificacion'], 'F.Alta+F.MOdif.');
-        
+
         // Filtros
         $activo = [
             ['code' => '1', 'description' => 'Activos = SI'],
@@ -47,28 +45,20 @@ class EditServiceRegularCombination extends EditController {
         $this->views[$viewName]->addFilterAutocomplete('xIdVehicle', 'vehicle', 'idvehicle', 'vehicles', 'idvehicle', 'nombre');
         $this->views[$viewName]->addFilterAutocomplete('xIdservice_regular', 'service-regular', 'idservice_regular', 'service_regulars', 'idservice_regular', 'nombre');
     }
-    
-    // function loadData es para cargar con datos las diferentes pestañas que tuviera el controlador
-    protected function loadData($viewName, $view) {
+
+    protected function loadData($viewName, $view)
+    {
+        $mvn = $this->getMainViewName();
         switch ($viewName) {
             case 'ListServiceRegularCombinationServ':
-                $idservice_regular_combination = $this->getViewModelValue('EditServiceRegularCombination', 'idservice_regular_combination');
+                $idservice_regular_combination = $this->getViewModelValue($mvn, 'idservice_regular_combination');
                 $where = [new DatabaseWhere('idservice_regular_combination', $idservice_regular_combination)];
                 $view->loadData('', $where);
                 break;
-                    
-            // Pestaña con el mismo nombre que este controlador EditXxxxx
-            case 'EditServiceRegularCombination':
-                parent::loadData($viewName, $view);
-                
-                // Guardamos que usuario y cuando pulsará guardar
-                $this->views[$viewName]->model->user_nick = $this->user->nick;
 
-             // $this->views[$viewName]->model->user_fecha = date('d-m-Y');
-                $this->views[$viewName]->model->user_fecha = date("Y-m-d H:i:s");
-                
+            default:
+                parent::loadData($viewName, $view);
                 break;
         }
     }
-    
 }
--- Model/ServiceRegularCombination.php
+++ Model/ServiceRegularCombination.php
@@ -1,124 +1,148 @@
 <?php
 
-namespace FacturaScripts\Plugins\OpenServBus\Model; 
+namespace FacturaScripts\Plugins\OpenServBus\Model;
 
 use FacturaScripts\Core\Model\Base;
+use FacturaScripts\Core\Session;
 
-class ServiceRegularCombination extends Base\ModelClass {
+class ServiceRegularCombination extends Base\ModelClass
+{
     use Base\ModelTrait;
 
-    public $idservice_regular_combination;
-        
-    public $user_fecha;
-    public $user_nick;
-    public $fechaalta;
-    public $useralta;
-    public $fechamodificacion;
-    public $usermodificacion;
+    /** @var bool */
     public $activo;
-    public $fechabaja;
-    public $userbaja;
-    public $motivobaja;
-    
-    
-    public $nombre;
-    
-    public $iddriver_1;
-    public $driver_alojamiento_1;
-    public $driver_observaciones_1;
-    
-    public $iddriver_2;
-    public $driver_alojamiento_2;
-    public $driver_observaciones_2;
-    
-    public $iddriver_3;
-    public $driver_alojamiento_3;
-    public $driver_observaciones_3;
-    
-    public $idvehicle;
-    
-    public $lunes;
-    public $martes;
-    public $miercoles;
-    public $jueves;
-    public $viernes;
-    public $sabado;
+
+    /** @var bool */
     public $domingo;
-    
+
+    /** @var int */
+    public $driver_alojamiento_1;
+
+    /** @var int */
+    public $driver_alojamiento_2;
+
+    /** @var int */
+    public $driver_alojamiento_3;
+
+    /** @var string */
+    public $driver_observaciones_1;
+
+    /** @var string */
+    public $driver_observaciones_2;
+
+    /** @var string */
+    public $driver_observaciones_3;
+
+    /** @var string */
+    public $fechaalta;
+
+    /** @var string */
+    public $fechabaja;
+
+    /** @var string */
+    public $fechamodificacion;
+
+    /** @var int */
+    public $iddriver_1;
+
+    /** @var int */
+    public $iddriver_2;
+
+    /** @var int */
+    public $iddriver_3;
+
+    /** @var int */
+    public $idservice_regular_combination;
+
+    /** @var int */
+    public $idvehicle;
+
+    /** @var bool */
+    public $jueves;
+
+    /** @var bool */
+    public $lunes;
+
+    /** @var bool */
+    public $martes;
+
+    /** @var bool */
+    public $miercoles;
+
+    /** @var string */
+    public $motivobaja;
+
+    /** @var string */
+    public $nombre;
+
+    /** @var string */
     public $observaciones;
 
-    
-    // función que inicializa algunos valores antes de la vista del controlador
-    public function clear() {
-        parent::clear();
-        $this->activo = true; // Por defecto estará activo
-    }
-    
-    /**
-     * This function is called when creating the model table. Returns the SQL
-     * that will be executed after the creation of the table. Useful to insert values
-     * default.
-     *
-     * @return string
-     */
-    public function install()
+    /** @var bool */
+    public $sabado;
+
+    /** @var string */
+    public $useralta;
+
+    /** @var string */
+    public $userbaja;
+
+    /** @var string */
+    public $usermodificacion;
+
+    /** @var bool */
+    public $viernes;
+
+    public function clear()
     {
-        /// needed dependency proveedores
+        parent::clear();
+        $this->activo = true;
+        $this->fechaalta = date(static::DATETIME_STYLE);
+        $this->useralta = Session::get('user')->nick ?? null;
+    }
+
+    public function install(): string
+    {
         new Driver();
         new Vehicle();
-
         return parent::install();
     }
 
-    // función que devuelve el id principal
-    public static function primaryColumn(): string {
+    public static function primaryColumn(): string
+    {
         return 'idservice_regular_combination';
     }
-    
-    // función que devuelve el nombre de la tabla
-    public static function tableName(): string {
+
+    public static function tableName(): string
+    {
         return 'service_regular_combinations';
     }
-    
-    // Para realizar cambios en los datos antes de guardar por modificación
-    protected function saveUpdate(array $values = [])
-    {
-        $this->rellenarDatosModificacion();
-        
-        if ($this->comprobarSiActivo() == false){
-            return false;
-        }
-        
-        return parent::saveUpdate($values);
-    }
 
-    // Para realizar cambios en los datos antes de guardar por alta
-    protected function saveInsert(array $values = [])
+    public function test(): bool
     {
-        // Creamos el nuevo id
-        if (empty($this->idservice_regular_combination)) {
-            $this->idservice_regular_combination = $this->newCode();
-        }
-        
-        $this->rellenarDatosAlta();
-        $this->rellenarDatosModificacion();
-        
-        if ($this->comprobarSiActivo() == false){
+        if ($this->comprobarSiActivo() === false) {
             return false;
         }
 
-        return parent::saveInsert($values);
-    }
-    
-    public function test()
-    {
-        $this->rellenarConductorVehiculoSiVacios();
-        
-        if ($this->hayServiciosQueNoCoincidenLosDiasDeSemana() == true) {
+        if (empty($this->iddriver_1) || empty($this->idvehicle)) {
+            $this->toolBox()->i18nLog()->info('Si no rellena el vehículo o el conductor, este será el orden de prioridades para el Montaje de Servicios:'
+                . ' 1º Combinación - Servicio Regular, 2º Combinación y 3º Servicio Regular');
+        }
+
+        if ($this->hayServiciosQueNoCoincidenLosDiasDeSemana() === true) {
             return false;
         }
-        
-        $this->evitarInyeccionSQL();
+
+        $utils = $this->toolBox()->utils();
+        $this->observaciones = $utils->noHtml($this->observaciones);
+        $this->nombre = $utils->noHtml($this->nombre);
+        $this->motivobaja = $utils->noHtml($this->motivobaja);
+        $this->driver_alojamiento_1 = $utils->noHtml($this->driver_alojamiento_1);
+        $this->driver_observaciones_1 = $utils->noHtml($this->driver_observaciones_1);
+        $this->driver_alojamiento_2 = $utils->noHtml($this->driver_alojamiento_2);
+        $this->driver_observaciones_2 = $utils->noHtml($this->driver_observaciones_2);
+        $this->driver_alojamiento_3 = $utils->noHtml($this->driver_alojamiento_3);
+        $this->driver_observaciones_3 = $utils->noHtml($this->driver_observaciones_3);
         return parent::test();
     }
 
@@ -127,23 +151,19 @@ class ServiceRegularCombination extends Base\ModelClass {
         return parent::url($type, $list . '?activetab=List');
     }
 
-
-    // ** ********************************** ** //
-    // ** FUNCIONES CREADAS PARA ESTE MODELO ** //
-    // ** ********************************** ** //
-    private function comprobarSiActivo()
+    protected function comprobarSiActivo()
     {
         $a_devolver = true;
-        
-        if ($this->activo == false) {
+        if ($this->activo === false) {
             $this->fechabaja = $this->fechamodificacion;
             $this->userbaja = $this->usermodificacion;
-            
-            if (empty($this->motivobaja)){
+
+            if (empty($this->motivobaja)) {
                 $a_devolver = false;
                 $this->toolBox()->i18nLog()->error('Si el registro no está activo, debe especificar el motivo.');
             }
-        } else { // Por si se vuelve a poner Activo = true
+        } else {
+            // Por si se vuelve a poner Activo = true
             $this->fechabaja = null;
             $this->userbaja = null;
             $this->motivobaja = null;
@@ -151,67 +171,29 @@ class ServiceRegularCombination extends Base\ModelClass {
         return $a_devolver;
     }
 
-    private function rellenarDatosModificacion()
-    {
-        $this->usermodificacion = $this->user_nick; 
-        $this->fechamodificacion = $this->user_fecha; 
-    }
-
-    private function rellenarDatosAlta()
-    {
-        $this->useralta = $this->user_nick; 
-        $this->fechaalta = $this->user_fecha; 
-    }
-
-    private function evitarInyeccionSQL()
-    {
-        $utils = $this->toolBox()->utils();
-        $this->observaciones = $utils->noHtml($this->observaciones);
-        $this->nombre = $utils->noHtml($this->nombre);
-        $this->motivobaja = $utils->noHtml($this->motivobaja);
-        
-        $this->driver_alojamiento_1 = $utils->noHtml($this->driver_alojamiento_1);
-        $this->driver_observaciones_1 = $utils->noHtml($this->driver_observaciones_1);
-        
-        $this->driver_alojamiento_2 = $utils->noHtml($this->driver_alojamiento_2);
-        $this->driver_observaciones_2 = $utils->noHtml($this->driver_observaciones_2);
-        
-        $this->driver_alojamiento_3 = $utils->noHtml($this->driver_alojamiento_3);
-        $this->driver_observaciones_3 = $utils->noHtml($this->driver_observaciones_3);
-    }
-    
-    private function rellenarConductorVehiculoSiVacios()
-    {
-        if (empty($this->iddriver_1) || empty($this->idvehicle)) {
-            $this->toolBox()->i18nLog()->info( 'Si no rellena el vehículo o el conductor, este será el orden de prioridades para el Montaje de Servicios:'
-                                             . ' 1º Combinación - Servicio Regular, 2º Combinación y 3º Servicio Regular' );
-        }
-    }
-    
-    private function hayServiciosQueNoCoincidenLosDiasDeSemana() : bool
+    protected function hayServiciosQueNoCoincidenLosDiasDeSemana(): bool
     {
         $serviciosConDiasDiferentes = [];
-        
-        $sql = ' SELECT service_regulars.lunes '
-             .      ' , service_regulars.martes '
-             .      ' , service_regulars.miercoles '
-             .      ' , service_regulars.jueves '
-             .      ' , service_regulars.viernes '
-             .      ' , service_regulars.sabado '
-             .      ' , service_regulars.domingo '
-             .      ' , service_regulars.idservice_regular '
-             .      ' , service_regulars.nombre '
-             . ' FROM service_regular_combination_servs '
-             . ' LEFT JOIN service_regulars on (service_regulars.idservice_regular = service_regular_combination_servs.idservice_regular) '
-             . ' WHERE service_regular_combination_servs.idservice_regular_combination = ' . $this->idservice_regular_combination
-             ;
 
-        $registros = self::$dataBase->select($sql); // Para entender su funcionamiento visitar ... https://facturascripts.com/publicaciones/acceso-a-la-base-de-datos-818
+        $sql = ' SELECT service_regulars.lunes '
+            . ' , service_regulars.martes '
+            . ' , service_regulars.miercoles '
+            . ' , service_regulars.jueves '
+            . ' , service_regulars.viernes '
+            . ' , service_regulars.sabado '
+            . ' , service_regulars.domingo '
+            . ' , service_regulars.idservice_regular '
+            . ' , service_regulars.nombre '
+            . ' FROM service_regular_combination_servs '
+            . ' LEFT JOIN service_regulars on (service_regulars.idservice_regular = service_regular_combination_servs.idservice_regular) '
+            . ' WHERE service_regular_combination_servs.idservice_regular_combination = ' . $this->idservice_regular_combination;
+
+        $registros = self::$dataBase->select($sql);
 
         foreach ($registros as $fila) {
             $coincideAlgunDia = false;
-            
-            // Una combinación puede tener varios servicios regulares, por lo 
+
+            // Una combinación puede tener varios servicios regulares, por lo
             // que tengo que comprobar todos sus servicios
             if ($this->lunes == 1) {
                 if ($this->lunes == $fila['lunes']) {
@@ -259,16 +241,21 @@ class ServiceRegularCombination extends Base\ModelClass {
                 $serviciosConDiasDiferentes[] = $fila['nombre'];
             }
         }
-        
+
         if (empty($serviciosConDiasDiferentes)) {
             return false;
-        } else {
-            foreach ($serviciosConDiasDiferentes as $servicio) {
-                $this->toolBox()->i18nLog()->error( "Los días de la semana del servicio $servicio no coinciden con los días de la semana de esta combinación." );
-            }
-            
-            return true;
         }
+
+        foreach ($serviciosConDiasDiferentes as $servicio) {
+            $this->toolBox()->i18nLog()->error("Los días de la semana del servicio $servicio no coinciden con los días de la semana de esta combinación.");
+        }
+        return true;
     }
-    
+
+    protected function saveUpdate(array $values = []): bool
+    {
+        $this->usermodificacion = Session::get('user')->nick ?? null;
+        $this->fechamodificacion = date(static::DATETIME_STYLE);
+        return parent::saveUpdate($values);
+    }
 }
